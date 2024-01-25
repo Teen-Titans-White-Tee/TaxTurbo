af8364110b65a518740ddfbf364025474de67f54
LNRtopher <143825175+LNRtopher@users.noreply.github.com>
2024-01-24 17:04:06 -0800

    configured store for redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = createRoot(document.querySelector('#root'));
-root.render (<App/>);
+root.render (
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  <App/>);
