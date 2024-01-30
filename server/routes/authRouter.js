05aba94eda922539ffe8e62e63dae9c74f3e6e0c
wantinglee90 <schwazle@gmail.com>
2024-01-29 23:02:06 -0500

    pulled dev

---
+++ server/routes/authRouter.js
@@ -0,0 +1,27 @@
+// Middleware for user signup & sign in
+const express = require('express')
+const authController = require('../Controllers/authController.js'); 
+const userController = require ('../Controllers/userController.js');
+const router = express.Router();
+const data = require('../Controllers/DataRetrieval.js')
+const calc = require('../Controllers/CalcController.js')
+
+//ROUTE FOR Login
+router.post ('/login', authController.loginUser, (req, res) => {
+  res.status(200).json({email, token});
+});
+
+router.get('/verify', 
+  authController.verifyToken, 
+  userController.findUser, 
+  (req, res) => {
+    console.log('verify endpoint')
+    return res.status(200).json(res.locals);
+});
+
+router.post ('/signup', userController.newUser ,data.stateBrackets, data.fedBrackets , calc.allTaxes, userController.createUser, (req, res) => {
+  res.status(200).json({ success: true, message: 'Request processed successfully', locals: res.locals });
+});
+
+
+module.exports = router;
