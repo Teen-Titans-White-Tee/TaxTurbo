6cdccdebf58b1068de76c4e4c21e1a9afc489824
wantinglee90 <schwazle@gmail.com>
2024-01-30 21:33:45 -0500

    dev

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,6 +1,5 @@
 // Middleware for user signup & sign in
 const express = require('express')
-<<<<<<< HEAD
 const authController = require('../Controllers/authController.js'); 
 const userController = require ('../Controllers/userController.js');
 const router = express.Router();
@@ -10,27 +9,13 @@ const calc = require('../Controllers/CalcController.js')
 //ROUTE FOR Login
 router.post ('/login', authController.loginUser, (req, res) => {
   res.status(200).json({email, token});
-=======
-const authController = require('../Controllers/authController'); 
-const userController = require ('../Controllers/userController.js');
-const router = express.Router()
-const data = require('../Controllers/DataRetrieval')
-const calc = require('../Controllers/CalcController')
-
-//ROUTE FOR Login
-router.post ('/login', authController.loginUser, (req, res) => {
-  return res.status(200).json({token: res.locals.token})
->>>>>>> 1de179896067e59183ff872c0fc34fa92ec05760
 });
 
 router.get('/verify', 
   authController.verifyToken, 
   userController.findUser, 
   (req, res) => {
-<<<<<<< HEAD
     console.log('verify endpoint')
-=======
->>>>>>> 1de179896067e59183ff872c0fc34fa92ec05760
     return res.status(200).json(res.locals);
 });
 
